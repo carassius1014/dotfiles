54930b5e81711f1a9e76f435b116043e74e53b36
carassius1014 <carassius1014@gmail.com>
2020-03-13 02:35:45 +0900

    [emacs] enable nix-mode

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -30,6 +30,7 @@
 (require 'prelude-magit)
 
 (require 'prelude-emacs-lisp)
+(require 'prelude-nix)
 
 (provide 'init)
 
@@ -41,7 +42,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (projectile neotree all-the-icons-dired all-the-icons-ivy all-the-icons which-key exec-path-from-shell evil-magit magit dashboard company counsel evil doom-themes use-package))))
+    (nix-mode editorconfig projectile neotree all-the-icons-dired all-the-icons-ivy all-the-icons which-key exec-path-from-shell evil-magit magit dashboard company counsel evil doom-themes use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
---
+++ emacs.d/prelude/prelude-nix.el
@@ -0,0 +1,7 @@
+;;; -*- lexical-binding: t; -*-
+
+(use-package nix-mode
+  :ensure t
+  :mode "\\.nix\\'")
+
+(provide 'prelude-nix)
