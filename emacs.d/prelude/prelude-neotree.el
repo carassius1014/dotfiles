b108507367a5906e9fa9d715115508a3a49c29e8
carassius1014 <carassius1014@gmail.com>
2020-03-13 05:52:23 +0900

    [emacs] neotree add evil keybinds

--- emacs.d/prelude/prelude-neotree.el
+++ emacs.d/prelude/prelude-neotree.el
@@ -4,6 +4,17 @@
   :ensure t
   :bind (([f8] . 'neotree-toggle))
   :config
-  (setq neo-theme (if (display-graphic-p) 'icons 'arrow)))
+  (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+  (add-hook 'neotree-mode-hook
+	    (lambda ()
+	      (define-key evil-normal-state-local-map (kbd "TAB") 'neotree-enter)
+	      (define-key evil-normal-state-local-map (kbd "SPC") 'neotree-quick-look)
+	      (define-key evil-normal-state-local-map (kbd "q") 'neotree-hide)
+	      (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)
+	      (define-key evil-normal-state-local-map (kbd "g") 'neotree-refresh)
+	      (define-key evil-normal-state-local-map (kbd "n") 'neotree-next-line)
+	      (define-key evil-normal-state-local-map (kbd "p") 'neotree-previous-line)
+	      (define-key evil-normal-state-local-map (kbd "A") 'neotree-stretch-toggle)
+	      (define-key evil-normal-state-local-map (kbd "H") 'neotree-hidden-file-toggle))))
 
 (provide 'prelude-neotree)
