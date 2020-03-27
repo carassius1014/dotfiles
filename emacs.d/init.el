17627b37f9e2d161c2d06b845ceadf8db56b6b67
carassius1014 <carassius1014@gmail.com>
2020-03-28 00:57:34 +0900

    [emacs] enable markdown mode

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -39,6 +39,7 @@
 (require 'prelude-fish)
 (require 'prelude-haskell)
 (require 'prelude-javascript)
+(require 'prelude-markdown)
 (require 'prelude-nix)
 (require 'prelude-ruby)
 (require 'prelude-yaml)
@@ -53,7 +54,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (yaml-mode doom-modeline flycheck nix-mode editorconfig projectile neotree all-the-icons-dired all-the-icons-ivy all-the-icons which-key exec-path-from-shell evil-magit magit dashboard company counsel evil doom-themes use-package))))
+    (markdown-mode yaml-mode doom-modeline flycheck nix-mode editorconfig projectile neotree all-the-icons-dired all-the-icons-ivy all-the-icons which-key exec-path-from-shell evil-magit magit dashboard company counsel evil doom-themes use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
