4ab1c63105fcc2527c9332f2929cc1362027ea23
carassius1014 <carassius1014@gmail.com>
2020-04-09 19:13:26 +0900

    [nixpkgs] refactor

--- config/nixpkgs/config.nix
+++ config/nixpkgs/config.nix
@@ -1,20 +1,20 @@
 let
-  nixpkgs = import ./channels/nixos.nix ;
+  nixos = import ./channels/nixos.nix ;
 in
 {
   allowUnfree = true;
   packageOverrides = pkgs: {
-    macosPackages = with nixpkgs; buildEnv {
+    macosPackages = with nixos; buildEnv {
       name = "macos-packages";
       paths = [
         coq
-        stack
         ghcid
         haskellPackages.brittany
         haskellPackages.hoogle
+        stack
       ];
     };
-    nixosPackages = with nixpkgs; buildEnv {
+    nixosPackages = with nixos; buildEnv {
       name = "nixos-packages";
       paths = [
         coq
