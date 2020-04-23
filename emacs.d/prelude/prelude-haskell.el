b6a3bdc786791f234daf94207b60e1310df88ecb
carassius1014 <carassius1014@gmail.com>
2020-04-24 03:48:29 +0900

    [emacs] add functions turn-{on/off}-haskell-format-on-save
    fix

--- emacs.d/prelude/prelude-haskell.el
+++ emacs.d/prelude/prelude-haskell.el
@@ -5,6 +5,16 @@
   :mode (("\\.hs\\'" . haskell-mode))
   :config
   (setq haskell-font-lock-symbols t)
-  (add-hook 'haskell-mode-hook 'haskell-auto-insert-module-template))
+  (add-hook 'haskell-mode-hook 'haskell-auto-insert-module-template)
+  (setq haskell-stylish-on-save nil)
+  (setq haskell-mode-stylish-haskell-path "ormolu"))
+
+(defun turn-on-haskell-format-on-save ()
+  (interactive)
+  (setq haskell-stylish-on-save t))
+
+(defun turn-off-haskell-format-on-save ()
+  (interactive)
+  (setq haskell-stylish-on-save nil))
 
 (provide 'prelude-haskell)
