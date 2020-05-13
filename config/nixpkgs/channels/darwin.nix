6668d8ecffa066bffd9b2e63a2e395affdc17320
carassius1014 <carassius1014@gmail.com>
2020-05-13 21:49:13 +0900

    [nixpkgs] bump darwin channel up to 20.03

--- config/nixpkgs/channels/darwin.nix
+++ config/nixpkgs/channels/darwin.nix
@@ -1,8 +1,8 @@
 let
   nixpkgs = builtins.fetchGit {
-    name = "nixpkgs-19.09-darwin";
+    name = "nixpkgs-20.03-darwin";
     url = "https://github.com/nixos/nixpkgs-channels/";
-    ref = "refs/heads/nixpkgs-19.09-darwin";
-    rev = "f6c1d3b113ce9e7c908991df2e8eed02f03df1bc";
+    ref = "refs/heads/nixpkgs-20.03-darwin";
+    rev = "fb5517f5c9e3fa12f038e6f1c2b677b0b040dd47";
   };
 in import nixpkgs { }
