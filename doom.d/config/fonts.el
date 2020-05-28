3aaaac4f377d22b866df006278379721edfabc24
carassius1014 <carassius1014@gmail.com>
2020-05-28 16:18:25 +0900

    [emacs] ajust appearance

--- doom.d/config/fonts.el
+++ doom.d/config/fonts.el
@@ -1,3 +1,3 @@
 ;;; ~/.dotfiles/doom.d/config/font.el -*- lexical-binding: t; -*-
 
-(setq doom-font (font-spec :family "Sarasa Mono CL" :size 16 :weight 'semi-light))
+(setq doom-font (font-spec :family "Sarasa Mono CL" :size 18 :weight 'semi-light))
--- doom.d/config/themes.el
+++ doom.d/config/themes.el
@@ -1,3 +1,3 @@
 ;;; ~/.dotfiles/doom.d/config/theme.el -*- lexical-binding: t; -*-
 
-(setq doom-theme 'doom-one-light)
+(setq doom-theme 'doom-opera-light)
