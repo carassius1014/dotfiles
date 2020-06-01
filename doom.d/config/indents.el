ce190d416c78a2b3f62fffca3b2b2d74bd4aefe7
carassius1014 <carassius1014@gmail.com>
2020-06-01 23:58:17 +0900

    [emacs] change highlight-indent-guides-method to 'column

--- doom.d/config/indents.el
+++ doom.d/config/indents.el
@@ -1,3 +1,6 @@
 ;;; ~/.dotfiles/doom.d/config/indents.el -*- lexical-binding: t; -*-
 
 (advice-add #'doom-highlight-non-default-indentation-h :override #'ignore)
+
+(after! highlight-indent-guides
+  (setq highlight-indent-guides-method 'column))
