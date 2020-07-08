9277a240cbb61703c8607a33d6c5f3f4a5edf9f4
carassius1014 <carassius1014@gmail.com>
2020-07-08 18:30:44 +0900

    [nixpkgs] bump up nixpkgs-unstable

--- config/nixpkgs/channels/nixpkgs.nix
+++ config/nixpkgs/channels/nixpkgs.nix
@@ -1,8 +1,8 @@
 let
   nixpkgs = builtins.fetchGit {
-    name = "nixpkgs-unstable-Jun-2020";
+    name = "nixpkgs-unstable-Jul-2020";
     url = "https://github.com/nixos/nixpkgs-channels/";
     ref = "refs/heads/nixpkgs-unstable";
-    rev = "00df2371122aeb96b4ca80451b1ac7b9b7b8d847";
+    rev = "4855aa62fa13052fb1a3daea68971503ab07a744";
   };
 in import nixpkgs { }
