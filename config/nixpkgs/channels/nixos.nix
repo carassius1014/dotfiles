5a224a4502628bc44249e5381bd11cf6a71f634e
carassius1014 <carassius1014@gmail.com>
2021-02-03 07:12:58 +0900

    [nixpkgs] Bump up nixos/nixpkgs

--- config/nixpkgs/channels/nixos.nix
+++ config/nixpkgs/channels/nixos.nix
@@ -1,8 +1,8 @@
 let
   nixpkgs = builtins.fetchGit {
-    name = "nixos-unstable-Jan-2021";
+    name = "nixos-unstable-Feb-2021";
     url = "https://github.com/NixOS/nixpkgs/";
     ref = "refs/heads/nixos-unstable";
-    rev = "733e537a8ad76fd355b6f501127f7d0eb8861775";
+    rev = "65211f5afcc3637c55423b327157a5eae05dff67";
   };
 in import nixpkgs { }
--- config/nixpkgs/channels/nixpkgs.nix
+++ config/nixpkgs/channels/nixpkgs.nix
@@ -1,8 +1,8 @@
 let
   nixpkgs = builtins.fetchGit {
-    name = "nixpkgs-unstable-Jan-2021";
+    name = "nixpkgs-unstable-Feb-2021";
     url = "https://github.com/NixOS/nixpkgs/";
     ref = "refs/heads/nixpkgs-unstable";
-    rev = "2080afd039999a58d60596d04cefb32ef5fcc2a2";
+    rev = "410bbd828cdc6156aecd5bc91772ad3a6b1099c7";
   };
 in import nixpkgs { }
