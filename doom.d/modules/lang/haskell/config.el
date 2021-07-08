fd8b79cc754df72465787c7eb52145436c9e05f3
carassius1014 <carassius1014@gmail.com>
2021-07-08 19:20:03 +0900

    [emacs] Fix haskell formatter to `brittany` temporarily

--- doom.d/modules/lang/haskell/config.el
+++ doom.d/modules/lang/haskell/config.el
@@ -7,11 +7,5 @@
    ("\\.y\\'" . fundamental-mode))
   :config
   (add-hook 'haskell-mode-hook 'haskell-auto-insert-module-template)
-  (add-hook 'buffer-list-update-hook 'setup-haskell-stylish-on-save))
-
-(defun setup-haskell-stylish-on-save ()
-  (let ((formatter (getenv "HASKELL_FORMATTER")))
-    (if formatter
-        (progn (setq haskell-mode-stylish-haskell-path formatter)
-               (setq haskell-stylish-on-save t))
-      (setq haskell-stylish-on-save nil))))
+  (setq haskell-stylish-on-save t)
+  (setq haskell-mode-stylish-haskell-path "brittany"))
