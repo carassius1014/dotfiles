d80fd5e40bf4652b1279e2ad6db12a4edef4ab15
carassius1014 <carassius1014@gmail.com>
2021-11-16 00:50:22 +0900

    [nixos] Add alternative haskell cache server

--- nixos/modules/cachix/haskell.nix
+++ nixos/modules/cachix/haskell.nix
@@ -1,9 +1,9 @@
 {
 
   nix = {
-    binaryCaches = [ "https://hydra.iohk.io" ];
+    binaryCaches = [ "https://cache.nixos.org" "https://hydra.iohk.io" ];
     binaryCachePublicKeys =
-      [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" ];
+      [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" ];
   };
 
 }
