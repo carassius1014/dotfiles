4d39176bdd4fd52e10ae06a52029313e444e012e
carassius1014 <carassius1014@gmail.com>
2021-12-09 12:50:08 +0900

    [nixos] Use stable emacs

--- nixos/modules/editor.nix
+++ nixos/modules/editor.nix
@@ -1,14 +1,5 @@
 { config, lib, pkgs, ... }:
 
 {
-  nixpkgs.overlays = [
-    (import (builtins.fetchTarball {
-      url =
-        "https://github.com/nix-community/emacs-overlay/archive/master.tar.gz";
-    }))
-  ];
-
-  services.emacs.package = with pkgs;
-    ((emacsPackagesNgGen emacsGcc).emacsWithPackages (epkgs: [ epkgs.vterm ]));
   services.emacs.enable = true;
 }
