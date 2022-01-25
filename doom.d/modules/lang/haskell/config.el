ea7170a85f8c7d8ce903e1cab337362fd9554c1d
carassius1014 <carassius1014@gmail.com>
2022-01-26 01:34:59 +0900

    [emacs] Setup haskell formatter by env var

--- doom.d/modules/lang/haskell/config.el
+++ doom.d/modules/lang/haskell/config.el
@@ -8,4 +8,12 @@
   :config
   (add-hook 'haskell-mode-hook 'haskell-auto-insert-module-template)
   (setq haskell-stylish-on-save t)
-  (setq haskell-mode-stylish-haskell-path "brittany"))
+  (haskell-formatter/reset))
+
+(defun haskell-formatter/reset ()
+  "Reset haskell formatter to $HASKELL_FORMATTER (default brittany)"
+  (interactive)
+  (let*
+      ((mformatter (getenv "HASKELL_FORMATTER"))
+       (formatter (if mformatter mformatter "brittany")))
+    (setq haskell-mode-stylish-haskell-path formatter)))
