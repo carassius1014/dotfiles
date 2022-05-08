b56d7143ba8a8437bff1d29a06c2106140ee4dd1
Jiyu Zhou <carassius1014@gmail.com>
2022-05-09 00:55:47 +0900

    [heitor] Rename submodule `configuration`

--- hosts/heitor/configuration.nix
+++ hosts/heitor/configuration.nix
@@ -3,23 +3,23 @@
 {
   imports = [
     ./hardware-configuration.nix
-    ./modules/audio.nix
-    ./modules/boot.nix
-    ./modules/cachix.nix
-    ./modules/console.nix
-    ./modules/docker.nix
-    ./modules/editor.nix
-    ./modules/firewall.nix
-    ./modules/fonts.nix
-    ./modules/i18n.nix
-    ./modules/network.nix
-    ./modules/nix.nix
-    ./modules/packages.nix
-    ./modules/ssh.nix
-    ./modules/system.nix
-    ./modules/terminal.nix
-    ./modules/users.nix
-    ./modules/video.nix
-    ./modules/xserver.nix
+    ./configuration/audio.nix
+    ./configuration/boot.nix
+    ./configuration/cachix.nix
+    ./configuration/console.nix
+    ./configuration/docker.nix
+    ./configuration/editor.nix
+    ./configuration/firewall.nix
+    ./configuration/fonts.nix
+    ./configuration/i18n.nix
+    ./configuration/network.nix
+    ./configuration/nix.nix
+    ./configuration/packages.nix
+    ./configuration/ssh.nix
+    ./configuration/system.nix
+    ./configuration/terminal.nix
+    ./configuration/users.nix
+    ./configuration/video.nix
+    ./configuration/xserver.nix
   ];
 }
