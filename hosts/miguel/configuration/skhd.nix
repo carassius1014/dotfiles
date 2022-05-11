5818dc5fd4a7a1c31e1ec2bfbf93044e529081da
Jiyu Zhou <carassius1014@gmail.com>
2022-05-11 21:31:48 +0900

    [miguel] Change `skhd` keybinds

--- hosts/miguel/configuration/skhd.nix
+++ hosts/miguel/configuration/skhd.nix
@@ -6,12 +6,10 @@
     package = pkgs.skhd;
     skhdConfig = ''
       # open terminal
-      cmd - return : kitty
+      lalt - return : kitty
 
       # open emacs
-      cmd - e : emacs
-      cmd + lalt -e : emacsclient --eval "(emacs-everywhere)"
-      cmd + shift -e : emacsclient --eval "(emacs-everywhere)"
+      lalt - e : emacs
 
       # focus window
       lalt - h : yabai -m window --focus west
@@ -26,30 +24,30 @@
       shift + lalt - k : yabai -m window --swap north
 
       # focus spaces
-      alt - x : yabai -m space --focus recent
-      alt - 1 : yabai -m space --focus 1
-      alt - 2 : yabai -m space --focus 2
-      alt - 3 : yabai -m space --focus 3
-      alt - 4 : yabai -m space --focus 4
-      alt - 5 : yabai -m space --focus 5
-      alt - 6 : yabai -m space --focus 6
-      alt - 7 : yabai -m space --focus 7
-      alt - 8 : yabai -m space --focus 8
+      lalt - x : yabai -m space --focus recent
+      lalt - 1 : yabai -m space --focus 1
+      lalt - 2 : yabai -m space --focus 2
+      lalt - 3 : yabai -m space --focus 3
+      lalt - 4 : yabai -m space --focus 4
+      lalt - 5 : yabai -m space --focus 5
+      lalt - 6 : yabai -m space --focus 6
+      lalt - 7 : yabai -m space --focus 7
+      lalt - 8 : yabai -m space --focus 8
 
       # focus on next/prev space
-      alt + ctrl - q : yabai -m space --focus prev
-      alt + ctrl - e : yabai -m space --focus next
+      lalt + ctrl - q : yabai -m space --focus prev
+      lalt + ctrl - e : yabai -m space --focus next
 
       # send window to desktop
-      shift + alt - x : yabai -m window --space recent
-      shift + alt - 1 : yabai -m window --space 1
-      shift + alt - 2 : yabai -m window --space 2
-      shift + alt - 3 : yabai -m window --space 3
-      shift + alt - 4 : yabai -m window --space 4
-      shift + alt - 5 : yabai -m window --space 5
-      shift + alt - 6 : yabai -m window --space 6
-      shift + alt - 7 : yabai -m window --space 7
-      shift + alt - 8 : yabai -m window --space 8
+      shift + lalt - x : yabai -m window --space recent
+      shift + lalt - 1 : yabai -m window --space 1
+      shift + lalt - 2 : yabai -m window --space 2
+      shift + lalt - 3 : yabai -m window --space 3
+      shift + lalt - 4 : yabai -m window --space 4
+      shift + lalt - 5 : yabai -m window --space 5
+      shift + lalt - 6 : yabai -m window --space 6
+      shift + lalt - 7 : yabai -m window --space 7
+      shift + lalt - 8 : yabai -m window --space 8
 
        # float / unfloat window and center on screen
       lalt - t : yabai -m window --toggle float;\
