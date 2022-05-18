785f43429de6808e0faf39c7b9d21db0085c74d5
Jiyu Zhou <carassius1014@gmail.com>
2022-05-18 19:04:25 +0900

    [miguel] Use `nix-direnv-flakes`

--- hosts/miguel/home.nix
+++ hosts/miguel/home.nix
@@ -3,10 +3,10 @@
 {
   imports = [
     ../common/home/programs
-    ../common/home/programs/direnv.nix
+  # ../common/home/programs/direnv.nix
     ../common/home/programs/fish.nix
     ../common/home/programs/git.nix
   ];
 
-  home.packages = with pkgs; [ gh jq python310Packages.editorconfig ripgrep ];
+  home.packages = with pkgs; [ gh jq nix-direnv-flakes python310Packages.editorconfig ripgrep ];
 }
