9fc5e32911da4e9e696208647c38fadaaf6897d5
Jiyu Zhou <carassius1014@gmail.com>
2022-07-12 00:23:55 +0900

    Enable `ghc` as a home module

--- hosts/miguel/home.nix
+++ hosts/miguel/home.nix
@@ -5,11 +5,11 @@
     ../common/home/programs
     ../common/home/programs/direnv.nix
     ../common/home/programs/fish.nix
+    ../common/home/programs/gh.nix
     ../common/home/programs/git.nix
   ];
 
   home.packages = with pkgs; [
-    gh
     jq
     nixfmt
     python310Packages.editorconfig
