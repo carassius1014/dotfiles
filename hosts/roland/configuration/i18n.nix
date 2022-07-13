079babfe1423369632a12871089705372c72506f
Jiyu Zhou <carassius1014@gmail.com>
2022-07-14 03:24:36 +0900

    [roland] No ime

--- hosts/roland/configuration/i18n.nix
+++ hosts/roland/configuration/i18n.nix
@@ -3,13 +3,7 @@
 {
   # Select internationalisation properties.
 
-  i18n = {
-    defaultLocale = "en_US.UTF-8";
-    inputMethod = {
-      enabled = "fcitx";
-      fcitx.engines = with pkgs.fcitx-engines; [ libpinyin mozc ];
-    };
-  };
+  i18n = { defaultLocale = "en_US.UTF-8"; };
 
   # Set your time zone.
   time.timeZone = "Asia/Tokyo";
