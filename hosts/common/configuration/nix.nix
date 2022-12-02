dc30ce6f5bdc6095ac64aa4740acb7529ef42d99
Jiyu Zhou <carassius1014@gmail.com>
2022-12-02 14:01:11 +0900

    Update cache substituters

--- hosts/common/configuration/nix.nix
+++ hosts/common/configuration/nix.nix
@@ -4,16 +4,16 @@
   nix = {
     settings = {
       substituters = [
+        "https://cache.iog.io"
         "https://cache.nixos.org"
         "https://herp-slum.cachix.org"
-        "https://hydra.iohk.io"
         "https://nix-community.cachix.org"
       ];
 
       trusted-public-keys = [
+        "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "herp-slum.cachix.org-1:6SC4HZSxqnmi6Jyxg+Omz+8o2uz8r4sgrz+cB1hn42I="
-        "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
 
