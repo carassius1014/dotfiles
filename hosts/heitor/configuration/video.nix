7023a46d098d157f3ab00f605586f5b7e0e5d736
Jiyu Zhou <carassius1014@gmail.com>
2022-12-07 02:44:27 +0900

    [heitor] Enable OpenGL

--- hosts/heitor/configuration/video.nix
+++ hosts/heitor/configuration/video.nix
@@ -1,6 +1,10 @@
 { pkgs, ... }:
 
 {
-  hardware.opengl.driSupport32Bit = true;
-  hardware.opengl.extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
+  hardware.opengl = {
+    enable = true;
+    driSupport32Bit = true;
+    extraPackages = [ pkgs.mesa.drivers ];
+    extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
+  };
 }
