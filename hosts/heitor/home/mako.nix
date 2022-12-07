678d3f534914c9b42f1f6df443bd49abe053963f
Jiyu Zhou <carassius1014@gmail.com>
2022-12-07 19:34:24 +0900

    [heitor] Configure mako ouput

--- hosts/heitor/home/mako.nix
+++ hosts/heitor/home/mako.nix
@@ -1,9 +1,12 @@
 { pkgs, ... }:
 
-let gctx = import ../../context.nix { inherit pkgs; };
+let
+  ctx = import ../context.nix { inherit pkgs; };
+  gctx = import ../../context.nix { inherit pkgs; };
 in {
   programs.mako = {
     enable = true;
     font = "${gctx.fonts.ui} 14";
+    output = ctx.monitors.dell-U2720QM;
   };
 }
