ee57940d8dd3ec55ac702c8bbcaac5cbf2b0b6a7
Jiyu Zhou <carassius1014@gmail.com>
2022-12-18 02:34:38 +0900

    [heitor] Configure vulkan driver

--- hosts/heitor/configuration/video.nix
+++ hosts/heitor/configuration/video.nix
@@ -1,12 +1,15 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   boot.initrd.kernelModules = [ "amdgpu" ];
 
   hardware.opengl = {
     enable = true;
+    driSupport = true;
     driSupport32Bit = true;
-    extraPackages = [ pkgs.mesa.drivers ];
-    extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
+    extraPackages = with pkgs; [ amdvlk rocm-opencl-icd rocm-opencl-runtime ];
+    extraPackages32 = with pkgs; [ driversi686Linux.amdvlk ];
   };
+
+  environment.variables.AMD_VULKAN_ICD = lib.mkDefault "RADV";
 }
