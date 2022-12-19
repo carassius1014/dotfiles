b7653c5acdccfdcd6ebd4feb0afb0bd5dd7b66e5
Jiyu Zhou <carassius1014@gmail.com>
2022-12-19 20:25:47 +0900

    [heitor] Run rofi command using fish

--- hosts/heitor/home/rofi.nix
+++ hosts/heitor/home/rofi.nix
@@ -9,5 +9,10 @@ in {
     package = pkgs.rofi-wayland;
     inherit (ctx) terminal;
     font = "${gctx.fonts.ui} 14";
+
+    extraConfig = {
+      run-command = "fish -c '{cmd}'";
+      run-list-command = "fish -c functions";
+    };
   };
 }
