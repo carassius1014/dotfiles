7d69389300899056034fb475d674f8c62b5bafd8
Jiyu Zhou <carassius1014@gmail.com>
2023-06-13 16:33:03 +0900

    [heitor] Add Haskell utilities

--- hosts/heitor/home/packages.nix
+++ hosts/heitor/home/packages.nix
@@ -1,36 +1,42 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [
-    black
-    dig
-    direnv
-    graphviz
-    fd
-    fzf
-    haskellPackages.hoogle
-    httpie
-    jq
-    libnotify
-    nix-prefetch-git
-    nodejs
-    okular
-    p7zip
-    plantuml
-    pdftk
-    python310
-    python310Packages.editorconfig
-    ripgrep
-    rlwrap
-    saml2aws
-    shellcheck
-    steam
-    symbola
-    texlive.combined.scheme-full
-    tree
-    unrar
-    unzip
-    whois
-    xdg-utils
-  ];
+  home.packages = with pkgs;
+    let ghcVersion = "ghc927";
+    in [
+      black
+      cabal-install
+      dig
+      direnv
+      graphviz
+      fd
+      fzf
+      haskell.compiler.${ghcVersion}
+      haskell.packages.${ghcVersion}.fourmolu
+      haskell.packages.${ghcVersion}.ghcid
+      haskell.packages.${ghcVersion}.hoogle
+      httpie
+      jq
+      libnotify
+      nix-prefetch-git
+      nodejs
+      okular
+      p7zip
+      plantuml
+      pdftk
+      python310
+      python310Packages.editorconfig
+      ripgrep
+      rlwrap
+      saml2aws
+      shellcheck
+      steam
+      symbola
+      texlive.combined.scheme-full
+      tree
+      unrar
+      unzip
+      whois
+      xdg-utils
+    ];
 }
