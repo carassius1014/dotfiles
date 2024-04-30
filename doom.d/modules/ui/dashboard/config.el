efc5a7f917acad584a17d21921bc532acbd38925
Jiyu Zhou <carassius1014@gmail.com>
2024-05-01 02:53:34 +0900

    [emacs] Fix dashboard

--- doom.d/modules/ui/dashboard/config.el
+++ doom.d/modules/ui/dashboard/config.el
@@ -1,10 +1,20 @@
 ;;; ui/dashboard/config.el -*- lexical-binding: t; -*-
-
 (use-package! dashboard
   :config
-  (dashboard-setup-startup-hook)
   (setq dashboard-center-content t)
-  (setq dashboard-set-footer nil)
-  (setq dashboard-items '((projects . 10)))
+  (setq dashboard-projects-backend 'projectile)
+  (setq dashboard-items
+    '((projects . 10)))
   (setq dashboard-set-heading-icons t)
-  (setq dashboard-set-file-icons t))
+  (setq dashboard-set-file-icons t)
+  (setq dashboard-startupify-list
+    '(dashboard-insert-banner
+       dashboard-insert-newline
+       dashboard-insert-banner-title
+       dashboard-insert-newline
+       dashboard-insert-navigator
+       dashboard-insert-newline
+       dashboard-insert-init-info
+       dashboard-insert-items
+       dashboard-insert-newline))
+  (dashboard-setup-startup-hook))
