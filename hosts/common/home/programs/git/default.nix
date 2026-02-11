a292e5fac6c98f7d0d7d66897ccbf97856967754
Jiyu Zhou <carassius1014@gmail.com>
2026-02-11 18:20:14 +0900

    Refine GitHub account settings

--- hosts/common/home/programs/git/default.nix
+++ hosts/common/home/programs/git/default.nix
@@ -3,6 +3,28 @@
 let
   inherit (pkgs) gctx;
 
+  ghCredentialHelper = pkgs.writeShellScript "gh-credential-helper" ''
+    if [ "$1" != "get" ]; then
+      exit 0
+    fi
+
+    path=""
+    while IFS= read -r line; do
+      case "$line" in
+        "") break ;;
+        path=*) path="''${line#path=}" ;;
+      esac
+    done
+
+    account="${gctx.me.name}"
+    case "$path" in
+      herp-inc-hq/*) account="carassius_herpinc" ;;
+    esac
+
+    token=$(${pkgs.gh}/bin/gh auth token --user "$account")
+    printf 'username=%s\npassword=%s\n' "$account" "$token"
+  '';
+
 in
 {
   programs.git = {
@@ -13,6 +35,13 @@ in
     extraConfig = {
       init = {
         defaultBranch = "master";
+      };
+      credential."https://github.com" = {
+        helper = "!${ghCredentialHelper}";
+        useHttpPath = true;
+      };
+      credential."https://gist.github.com" = {
+        helper = "!${ghCredentialHelper}";
       };
     };
   };
